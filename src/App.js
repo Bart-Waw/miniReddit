2bd7cd1adc4a57b36124675277636c2b33865871
Bart-Waw <wawrejkob@gmail.com>
2020-12-30 19:13:40 +0000

    removed unnecesary code

--- src/App.js
+++ src/App.js
@@ -3,17 +3,6 @@ import { Search } from './Search.js';
 import { Display } from './Display.js';
 
 function App () {
-  /*
-
-  
-
-  function returnButton () {
-    let newList = listMakerTen(myReddit);
-    setDisplay(newList);
-  };
-
-
-*/
   return (
     <div id="main">
       <h1 id="pageTitle">Mini Reddit</h1>
